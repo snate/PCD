a4d585baa7c6254657c3d9413dc6874ef9f52f4a
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-01-27 16:36:51 +0100

    PCD: Aggiunta del Client al Driver

---
+++ consegne/parte3/puzzle/Driver.java
@@ -0,0 +1,34 @@
+package puzzle;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+/**
+ * @author svalle
+ */
+public class Driver {
+    public static void main(String[] args) {
+		String pathIn = args[0]; 					//primo argomento passato alla consolle
+    String pathOut = args[1];         //secondo argomento passato alla consolle
+    String nomeServer = args[2];         //terzo argomento passato alla consolle
+		Path inputPath = Paths.get(pathIn);
+		Path outputPath = Paths.get(pathOut);
+		Test test = new Test();
+		inputPath = test.build(inputPath);
+        if(args.length > 0)
+            args[0] = inputPath.toString();
+    String[] serverArgs = new String[1]; serverArgs[0] = nomeServer;
+		PuzzleSolverServer.main(serverArgs);
+    PuzzleSolverClient.main(args);
+		Path log = Paths.get("log");
+		Date now = new Date();
+		if(test.checkOut(outputPath)) {
+			InputOutput.writeContent(log, "OK: " + now);
+			System.out.println("OK");
+		}
+		else {
+			InputOutput.writeContent(log, "ERRORE: " + now);
+			System.out.println("ERRORE");
+		}
+    }
+}
